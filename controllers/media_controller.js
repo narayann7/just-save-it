684ddb4870f603ce246a77297528f6e096884eb6
narayann7 <laxminarayanreddy432@gmail.com>
2021-09-17 21:47:16 +0530

    fixed more bugs

--- controllers/media_controller.js
+++ controllers/media_controller.js
@@ -4,29 +4,36 @@ const reelDownloader = require("../models/reel_downloader");
 const basic_command = {
   async reelDownload(context) {
     try {
-      // if(context.message.text=="/download_reel"&& (context.message.reply_to_message.chat.id))
-      // context.telegram.sendMessage(context.message.chat.id,
-      //
-      //   )
+      console.log(context.message.reply_to_message);
+      if (
+        context.message.text == "/download_reel" &&
+        context.message.reply_to_message.chat.id == undefined
+      )
+        context.telegram.sendMessage(
+          context.message.chat.id,
+          `only "/download_reel"  command will not work\nclick -> /video_tutorial for tutorial
+        `
+        );
+      else {
+        const result = await reelDownloader(
+          context.message.reply_to_message.text
+        );
 
-      const result = await reelDownloader(
-        context.message.reply_to_message.text
-      );
-
-      const link = result.video_url;
-      context.telegram.sendChatAction(
-        context.message.reply_to_message.chat.id,
-        "upload_video"
-      );
-      context.telegram.sendVideo(
-        context.message.reply_to_message.chat.id,
-        link
-      );
+        const link = result.video_url;
+        context.telegram.sendChatAction(
+          context.message.reply_to_message.chat.id,
+          "upload_video"
+        );
+        context.telegram.sendVideo(
+          context.message.reply_to_message.chat.id,
+          link
+        );
+      }
     } catch (error) {
       if (context.message.reply_to_message == undefined)
         context.telegram.sendMessage(
-          context.message.reply_to_message.chat.id,
-          `only "/download_reel"  command will not work\nclick -> /video_tutorial for tutorial
+          context.message.chat.id,
+          `only "/download_reel"  command will not work\n\nclick -> /video_tutorial for tutorial
          `
         );
       else
@@ -39,25 +46,30 @@ const basic_command = {
   async videoTutorial(context) {
     context.telegram.sendVideo(
       context.message.chat.id,
-      "BAACAgUAAxkBAANSYURjZ-OwHV0htFyvqlV2Gs5aoEQAAkgCAAJDEilWxCk3TPrgVcUgBA"
+      "BAACAgUAAxkBAAIBhmFEukyUVg-pEbu49d-TwbX4cxciAAL3AwACeskoVgv9suvZcwrHIAQ"
     );
   },
   help(context) {
     context.reply(
       `HOW TO DOWNLOAD REELS :
 
+click on this /video_tutorial for video tutorial
+
+
 1. copy the reel link 🔗 from instagram and paste here 
     and send it to the bot.
-2. then swipe the link or press it or hold for a while then reply
+
+2. then swipe the link or press it or hold for a while then
+
 3. type  "/download_reel" , reply back to link and send it✨
       
-     or click on this /video_tutorial for video tutorial
 
 NOTE: 
 
 1. for now you only can download reels 
     and video posts 
-    ig tv videos with >50mb cant be downloaded.
+    ig tv videos with >50mb and 
+    reels from private account cant be downloaded.
 
 2. downloading facebook videos , instagram stories , 
     picture will added later . 
@@ -79,13 +91,20 @@ bot is just for 👨‍🎓 educational purpose..
 recently learing 👨‍💻 Server-side programming using nodeJs
 thought to build 🏗️ something useful.
 
+hope you like it 👀
+
 source code - "https://github.com/narayann7/just-save-it.git" 
 
 if any thing went wrong then type 
 /error "with error messsage" 
 and send
 
+want to contact,  ping -> @skillZ6
+
 Never settle and keep learning!!
+
+......................
+
 
     `
     );
@@ -94,12 +113,10 @@ Never settle and keep learning!!
     if (c.message.text == "/error")
       c.telegram.sendMessage(
         c.message.chat.id,
-        ` like this -> " /error with some message" `
+        ` send like this -> " /error with some message" `
       );
     else c.telegram.sendMessage(process.env.BOT_TOKEN_MAIN, c.message.text);
   },
 };
 
 module.exports = basic_command;
-
-// https://github.com/ishan0445/telegraf-telegram-bot-deploy
